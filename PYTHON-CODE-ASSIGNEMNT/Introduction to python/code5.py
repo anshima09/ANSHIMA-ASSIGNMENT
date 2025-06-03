5c40b7b3245ec4e89e1f97f54e941b81464b5fcc
Anshima Sharma <anshima0906s@gmail.com>
2025-06-03 11:58:10 +0530

    folder deleted

--- PYTHON-CODE-ASSIGNEMNT/Introduction to python/code5.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter value for a: "))
-b = int(input("Enter value for b: "))
-
-
-print(f"Before swapping: a = {a}, b = {b}")
-
-a, b = b, a
-
-print(f"After swapping: a = {a}, b = {b}")
--- PYTHON-CODE-ASSIGNEMNT/Introduction to python/code9.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter the number of Fibonacci terms: "))
-
-a, b = 0, 1
-count = 0
-
-while count < number:
-    print(a, end=" ")
-    a, b = b, a + b
-    count += 1
